a75ed70422476dc8683257fa02e8d386b78e3793
Thuy Ngoc <ngoc.maithuy@ncc.asia>
2022-07-19 13:45:42 +0700

    update columns name table status_labels

--- database/migrations/2022_07_19_064036_update_name_status_labels_table.php
+++ database/migrations/2022_07_19_064036_update_name_status_labels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateColumnNameTableStatusLabels extends Migration
+class UpdateNameStatusLabelsTable extends Migration
 {
     /**
      * Run the migrations.
