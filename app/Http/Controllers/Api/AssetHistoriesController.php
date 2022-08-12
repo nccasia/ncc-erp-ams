ef50b09ab283e61baaa0849f66ce262d22362e45
Anh Thu <thu.lethianh.ncc@gmail.com>
2022-08-12 15:32:24 +0700

    update search

--- app/Http/Controllers/Api/AssetHistoriesController.php
+++ app/Http/Controllers/Api/AssetHistoriesController.php
@@ -3,104 +3,76 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Asset;
 use App\Models\AssetHistoryDetail;
 use Illuminate\Http\Request;
-
-use function PHPUnit\Framework\isNull;
+use App\Models\Company;
 
 class AssetHistoriesController extends Controller
 {
     public function index(Request $request)
     {
-        // for search
-        $type = $request->assetHistoryType;
-        $from = $request->purchaseDateFrom;
-        $to = $request->purchaseDateTo;
-        $location = $request->location;
-        $category = $request->category_id;
-        $name = $request->name;
-
-        // for paginate
-        $offset = $request->offset;
-        $limit = $request->limit;
+        // for seach : type, date_from, date_to, location_id, category_id, model_id, assigned_status, status_id
+        // for paginate : offset, limit
+        // for sort : sort, order: asc - desc
 
         // show all when api have NO params
-        $histories = AssetHistoryDetail::with(['asset', 'asset_history', 'asset_history.user:id,first_name,last_name,username']);
+        $histories = AssetHistoryDetail::query();
 
-        if (!is_null($type) || !is_null($from) || !is_null($to) || !is_null($location) || !is_null($category) || !is_null($name)) {
-            $assetHistoryQuery = AssetHistoryDetail::whereHas('asset_history', function ($query) use ($type, $from, $to) {
-                $query->where('type', $type);
-                if (!is_null($from)) {
-                    $query->whereDate('created_at', '>=', $from);
-                }
+        $histories->join('assets', 'asset_history_details.asset_id', '=', 'assets.id')
+            ->join('asset_histories', 'asset_history_details.asset_histories_id', '=', 'asset_histories.id');
 
-                if (!is_null($to)) {
-                    $query->whereDate('created_at', '<=', $to);
-                }
-            });
+        if ($request->filled('type')) {
+            $histories->where('type', $request->input('type'));
+        }
 
-            if (!is_null($type)) {
-                // show histories depend on type when api only have 'assetHistoryType' param
-                $histories = $assetHistoryQuery->with(['asset', 'asset_history', 'asset_history.user:id,first_name,last_name,username']);
+        if ($request->filled('date_from')) {
+            $histories->whereDate('asset_history_details.created_at', '>=', $request->input('date_from'));
+        }
 
-                if (!is_null($from) || !is_null($to) || !is_null($location) || !is_null($category) || !is_null($name)) {
-                    $histories = $assetHistoryQuery
-                        ->whereHas('asset', function ($query) use ($location, $category, $name) {
-                            if (!is_null($location)) {
-                                $query->where('rtd_location_id', $location);
-                            }
+        if ($request->filled('date_to')) {
+            $histories->whereDate('asset_history_details.created_at', '<=', $request->input('date_to'));
+        }
 
-                            if (!is_null($category)) {
-                                $query->join('models', 'assets.model_id', '=', 'models.id')
-                                    ->select('category_id')
-                                    ->where('category_id', $category);
-                            }
+        if ($request->filled('location_id')) {
+            $histories->where('rtd_location_id', $request->input('location_id'));
+        }
 
-                            if (!is_null($name)) {
-                                $query->where('name', $name);
-                            }
-                        })->with(['asset', 'asset_history', 'asset_history.user:id,first_name,last_name,username']);
-                }
-            } else {
-                // when params do not have 'assetHistoryType' param
-                $histories = AssetHistoryDetail::whereHas('asset', function ($query) use ($from, $to, $location, $category, $name) {
-                    if (!is_null($from)) {
-                        $query->where('purchase_date', '>=', $from);
-                    }
+        if ($request->filled('model_id')) {
+            $histories->InModelList([$request->input('model_id')]);
+        }
 
-                    if (!is_null($to)) {
-                        $query->where('purchase_date', '<=', $to);
-                    }
+        if ($request->filled('category_id')) {
+            $histories->InCategory($request->input('category_id'));
+        }
 
-                    if (!is_null($location)) {
-                        $query->where('rtd_location_id', $location);
-                    }
+        if ($request->filled('assigned_status')) {
+            $histories->where('assigned_status', '=', $request->input('assigned_status'));
+        }
 
-                    if (!is_null($category)) {
-                        $query->join('models', 'assets.model_id', '=', 'models.id')
-                            ->select('category_id')
-                            ->where('category_id', $category);
-                    }
+        if ($request->filled('status_id')) {
+            $histories->where('status_id', '=', $request->input('status_id'));
+        }
 
-                    if (!is_null($name)) {
-                        $query->where('name', $name);
-                    }
-                })->with(['asset', 'asset_history', 'asset_history.user:id,first_name,last_name,username']);
-            }
-
-            $assetHistoryQuery = AssetHistoryDetail::whereHas('asset', function ($query) use ($name) {
-                $query->where('name', $name);
-            });
+        if ($request->filled('search')) {
+            $histories->TextSearch($request->input('search'));
         }
 
         // Set the offset to the API call's offset, unless the offset is higher than the actual count of items in which
         // case we override with the actual count, so we should return 0 items.
-        $offset = (($histories->get()) && ($offset > $histories->count())) ? $histories->count() : $offset;
+        $offset = (($histories->get()) && ($request->get('offset') > $histories->count())) ? $histories->count() : $request->get('offset', 0);
 
         // Check to make sure the limit is not higher than the max allowed
-        $limit = ((config('app.max_results') >= $limit) && $limit) ? $limit : config('app.max_results');
-        $histories = (!is_null($offset) && !is_null($limit)) ? $histories->skip($offset)->take($limit)->get() : $histories->get();
+        ((config('app.max_results') >= $request->input('limit')) && ($request->filled('limit')))
+            ? $limit = $request->input('limit') : $limit = config('app.max_results');
+
+        $get_histories = [
+            'asset_history_details.*',
+            'assets.name', 'assets.asset_tag', 'assets.notes',
+            'asset_histories.assigned_to', 'asset_histories.user_id', 'asset_histories.type'
+        ];
+
+        $histories = ($request->filled('offset') && $request->filled('limit'))
+            ? $histories->skip($offset)->take($limit)->get($get_histories) : $histories->get($get_histories);
 
         return $histories;
     }
--- app/Models/AssetHistoryDetail.php
+++ app/Models/AssetHistoryDetail.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\Searchable;
+use DB;
 
 class AssetHistoryDetail extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $table = 'asset_history_details';
     protected $fillable = [
@@ -23,5 +27,38 @@ class AssetHistoryDetail extends Model
     public function asset()
     {
         return $this->belongsTo(\App\Models\Asset::class, 'asset_id');
+    }
+
+    /**
+     * -----------------------------------------------
+     * BEGIN QUERY SCOPES
+     * -----------------------------------------------
+     **/
+
+    public function advancedTextSearch(Builder $query, array $terms)
+    {        
+        $query = $query->leftJoin('users', 'users.id', '=', 'asset_histories.assigned_to');
+     
+        foreach ($terms as $term) {  
+                $query = $query->where(function ($query) use ($term) {
+                    $query->orwhere('users.first_name', 'LIKE', '%' . $term . '%')
+                    ->orWhere('users.last_name', 'LIKE', '%' . $term . '%')
+                    ->orWhere('users.username', 'LIKE', '%' . $term . '%')
+                    ->orWhereRaw('CONCAT(' . DB::getTablePrefix() . 'users.first_name," ",' . DB::getTablePrefix() . 'users.last_name) LIKE ?', ["%$term%"]);
+                });
+        }
+
+        return $query;
+    }
+
+    public function scopeInModelList($query, array $modelIdListing)
+    {
+        return $query->whereIn('assets.model_id', $modelIdListing);
+    }
+
+    public function scopeInCategory($query, $category_id)
+    {
+        return $query->join('models as category_models', 'assets.model_id', '=', 'category_models.id')
+            ->join('categories', 'category_models.category_id', '=', 'categories.id')->where('category_models.category_id', '=', $category_id);
     }
 }
