6f22607f85df9c176429f3700071e30d83b47e07
Anh Thu <thu.lethianh.ncc@gmail.com>
2022-08-15 10:26:37 +0700

    update search and sort for api

--- app/Http/Controllers/Api/AssetHistoriesController.php
+++ app/Http/Controllers/Api/AssetHistoriesController.php
@@ -5,20 +5,27 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\AssetHistoryDetail;
 use Illuminate\Http\Request;
-use App\Models\Company;
 
 class AssetHistoriesController extends Controller
 {
     public function index(Request $request)
     {
-        // for seach : type, date_from, date_to, location_id, category_id, model_id, assigned_status, status_id
         // for paginate : offset, limit
         // for sort : sort, order: asc - desc
 
         // show all when api have NO params
-        $histories = AssetHistoryDetail::query();
+        // $histories = AssetHistoryDetail::query();
+        $histories = AssetHistoryDetail::select('asset_history_details.*');
 
-        $histories->join('assets', 'asset_history_details.asset_id', '=', 'assets.id')
+        $histories->with(
+            'asset',
+            'asset_history',
+            'asset.location:id,name',
+            'asset.assetstatus:id,name',
+            'asset.model.category:id,name',
+            'asset.assignedTo:first_name,last_name'
+
+        )->join('assets', 'asset_history_details.asset_id', '=', 'assets.id')
             ->join('asset_histories', 'asset_history_details.asset_histories_id', '=', 'asset_histories.id');
 
         if ($request->filled('type')) {
@@ -57,6 +64,57 @@ class AssetHistoriesController extends Controller
             $histories->TextSearch($request->input('search'));
         }
 
+        $allowed_columns = [
+            'id',
+            'created_at',
+            'name',
+            'asset_tag',
+            'rtd_location',
+            'category',
+            'assigned_to'
+        ];
+
+        $order = $request->input('order') === 'asc' ? 'asc' : 'desc';
+
+        // This is kinda gross, but we need to do this because the Bootstrap Tables
+        // API passes custom field ordering as custom_fields.fieldname, and we have to strip
+        // that out to let the default sorter below order them correctly on the assets table.
+        $sort_override = str_replace('custom_fields.', '', $request->input('sort'));
+
+        // This handles all of the pivot sorting (versus the assets.* fields
+        // in the allowed_columns array)
+        $column_sort = in_array($sort_override, $allowed_columns) ? $sort_override : 'created_at';
+
+        switch ($sort_override) {
+            case 'id':
+                $histories->OrderIds($order);
+                break;
+            case 'created_at':
+                $histories->OrderCreatedAt($order);
+                break;
+            case 'name':
+                $histories->OrderName($order);
+            case 'asset_tag':
+                $histories->OrderAssetTag($order);
+            case 'rtd_location':
+                $histories->OrderRtdLocation($order);
+                break;
+            case 'category':
+                $histories->OrderCategory($order);
+                break;
+            case 'assigned_to':
+                $histories->OrderAssignedTo($order);
+                break;
+            default:
+                $priority_assign_status = config('enum.assigned_status.WAITING');
+                $histories->orderByRaw(
+                    "CASE WHEN assets.assigned_status = $priority_assign_status THEN 1 ELSE 0 END DESC"
+                );
+                $histories->orderBy($column_sort, $order);
+                break;
+        }
+
+
         // Set the offset to the API call's offset, unless the offset is higher than the actual count of items in which
         // case we override with the actual count, so we should return 0 items.
         $offset = (($histories->get()) && ($request->get('offset') > $histories->count())) ? $histories->count() : $request->get('offset', 0);
@@ -65,14 +123,9 @@ class AssetHistoriesController extends Controller
         ((config('app.max_results') >= $request->input('limit')) && ($request->filled('limit')))
             ? $limit = $request->input('limit') : $limit = config('app.max_results');
 
-        $get_histories = [
-            'asset_history_details.*',
-            'assets.name', 'assets.asset_tag', 'assets.notes',
-            'asset_histories.assigned_to', 'asset_histories.user_id', 'asset_histories.type'
-        ];
-
         $histories = ($request->filled('offset') && $request->filled('limit'))
-            ? $histories->skip($offset)->take($limit)->get($get_histories) : $histories->get($get_histories);
+            ? $histories->skip($offset)->take($limit)->get() : $histories->get();
+
 
         return $histories;
     }
--- app/Models/AssetHistoryDetail.php
+++ app/Models/AssetHistoryDetail.php
@@ -36,16 +36,24 @@ class AssetHistoryDetail extends Model
      **/
 
     public function advancedTextSearch(Builder $query, array $terms)
-    {        
-        $query = $query->leftJoin('users', 'users.id', '=', 'asset_histories.assigned_to');
-     
-        foreach ($terms as $term) {  
-                $query = $query->where(function ($query) use ($term) {
-                    $query->orwhere('users.first_name', 'LIKE', '%' . $term . '%')
+    {
+        $query = $query->leftJoin('locations', 'locations.id', '=', 'assets.rtd_location_id')
+            ->leftJoin('status_labels', 'status_labels.id', '=', 'assets.status_id')
+            ->leftJoin('models', 'models.id', '=', 'assets.model_id')
+            ->leftJoin('categories', 'categories.id', '=', 'models.category_id')
+            ->leftJoin('users', 'users.id', '=', 'asset_histories.assigned_to');
+
+        foreach ($terms as $term) {
+            $query = $query->where(function ($query) use ($term) {
+                $query->orwhere('assets.name', 'LIKE', '%' . $term . '%')
+                    ->orWhere('assets.asset_tag', 'LIKE', '%' . $term . '%')
+                    ->orWhere('categories.name', 'LIKE', '%' . $term . '%')
+                    ->orWhere('locations.name', 'LIKE', '%' . $term . '%')
+                    ->orWhere('status_labels.name', 'LIKE', '%' . $term . '%')->orwhere('users.first_name', 'LIKE', '%' . $term . '%')
                     ->orWhere('users.last_name', 'LIKE', '%' . $term . '%')
                     ->orWhere('users.username', 'LIKE', '%' . $term . '%')
                     ->orWhereRaw('CONCAT(' . DB::getTablePrefix() . 'users.first_name," ",' . DB::getTablePrefix() . 'users.last_name) LIKE ?', ["%$term%"]);
-                });
+            });
         }
 
         return $query;
@@ -60,5 +68,43 @@ class AssetHistoryDetail extends Model
     {
         return $query->join('models as category_models', 'assets.model_id', '=', 'category_models.id')
             ->join('categories', 'category_models.category_id', '=', 'categories.id')->where('category_models.category_id', '=', $category_id);
+    }
+
+    // scope for sort
+    public function scopeOrderIds($query, $order)
+    {
+        return $query->orderBy('asset_history_details.id', $order);
+    }
+
+    public function scopeOrderCreatedAt($query, $order)
+    {
+        return $query->orderBy('asset_history_details.created_at', $order);
+    }
+
+    public function scopeOrderName($query, $order)
+    {
+        return $query->orderBy('assets.name', $order);
+    }
+
+    public function scopeOrderAssetTag($query, $order)
+    {
+        return $query->orderBy('assets.asset_tag', $order);
+    }
+
+    public function scopeOrderRtdLocation($query, $order)
+    {
+        return $query->orderBy('assets.rtd_location_id', $order);
+    }
+
+    public function scopeOrderCategory($query, $order)
+    {
+        return $query->join('models', 'assets.model_id', '=', 'models.id')
+            ->join('categories', 'models.category_id', '=', 'categories.id')
+            ->orderBy('categories.id', $order);
+    }
+
+    public function scopeOrderAssignedTo($query, $order)
+    {
+        return $query->leftJoin('users as users_sort', 'asset_histories.assigned_to', '=', 'users_sort.id')->orderBy('users_sort.first_name', $order)->orderBy('users_sort.last_name', $order);
     }
 }
