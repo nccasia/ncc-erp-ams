f6c190d5df26d885dd193aede8cbdc0b07567e5e
Anh Thu <thu.lethianh.ncc@gmail.com>
2022-08-22 10:43:40 +0700

    add category search

--- app/Http/Controllers/Api/ReportsController.php
+++ app/Http/Controllers/Api/ReportsController.php
@@ -55,7 +55,8 @@ class ReportsController extends Controller
         }
 
         if ($request->filled('category_id')) {
-            $actionlogs->leftJoin('models', 'assets.model_id', '=', 'models.id')->leftJoin('categories', 'models.category_id', '=', 'categories.id')->where('models.category_id', $request->input('category_id'));
+            $actionlogs->leftJoin('models', 'assets.model_id', '=', 'models.id')
+                ->where('models.category_id', $request->input('category_id'));
         }
 
         // For sort
