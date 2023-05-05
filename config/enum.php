9a070f60954e73d1922141bb3052429dbabd036c
khanh.tranvan <khanh.tranvan@ncc.asia>
2023-05-05 08:37:40 +0700

    unchange emum file(constants folder)

--- config/enum.php
+++ config/enum.php
@@ -11,30 +11,25 @@ return [
     "WAITING" => 1, 
     "ACCEPT"  => 2,  
     "REJECT"  => 3,
-    "REJECTREVOKE"  => 6,
     "WAITINGCHECKOUT" => 4, 
     "WAITINGCHECKIN" => 5, 
   ],
   "status_id" => [
-    "READY_TO_DEPLOY" => 1,
-    "PENDING" => 2,
-    "ARCHIVED" => 3,
-    "OUT_FOR_DIAGNOSTICS" => 4,
-    "OUT_FOR_REPAIR" => 5,
-    "BROKEN_NOT_FIXABLE" => 6,
-    "LOST_STOLEN" => 7,
-    "ASSIGN" => 8,
+    "PENDING" => 1,
+    "BROKEN" => 3,
+    "ASSIGN" => 4,
+    "READY_TO_DEPLOY" => 5,
   ],
   "asset_history" => [
     "CHECK_IN_TYPE" => 1,
     "CHECK_OUT_TYPE" => 0
   ],
-  "seats" =>[
-    "MIN" => 0,
-  ],
   "permission_status" => [
     "ALLOW" => 1,
     "REFUSE" => -1,
     "INHERITANCE" => 0,
-  ]
+  ],
+  "seats" =>[
+    "MIN" => 0,
+  ],
 ];
