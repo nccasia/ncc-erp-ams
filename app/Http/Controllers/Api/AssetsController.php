463499a6c0bd72bb9ac09373b72433d4ff5134fa
banhan14999 <huynhbanhan1491999@gmail.com>
2023-08-01 13:37:49 +0700

    Fix response code

--- app/Http/Controllers/Api/AssetsController.php
+++ app/Http/Controllers/Api/AssetsController.php
@@ -1539,7 +1539,7 @@ class AssetsController extends Controller
                 return response()->json(Helper::formatStandardApiResponse('success', $asset, trans('admin/hardware/message.update.success')));
             }
 
-            return response()->json(Helper::formatStandardApiResponse('error', null, $asset->getErrors()), 200);
+            return response()->json(Helper::formatStandardApiResponse('error', null, $asset->getErrors()), 422);
         }
 
         return response()->json(Helper::formatStandardApiResponse('error', null, trans('admin/hardware/message.does_not_exist')), 200);
